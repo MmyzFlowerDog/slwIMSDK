1d5027f3fb5fa6acfaee04ba6f9b10d2dfb53f61
duoyi <duoyi@duoyis-MacBook-Air-4.local>
2019-12-26 10:59:49 +0800

    [Add]:Upload project and use protocol buffer

---
+++ HXIMSDK/HXIMSDK.h
@@ -0,0 +1,15 @@
+//
+//  HXIMSDK.h
+//  HXIMSDK
+//
+//  Created by slw on 2019/12/3.
+//  Copyright © 2019 slw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HXIMSDK : NSObject
+
+
+
+@end
---
+++ HXIMSDK/HXIMSDK.m
@@ -0,0 +1,13 @@
+//
+//  HXIMSDK.m
+//  HXIMSDK
+//
+//  Created by slw on 2019/12/3.
+//  Copyright © 2019 slw. All rights reserved.
+//
+
+#import "HXIMSDK.h"
+
+@implementation HXIMSDK
+
+@end
